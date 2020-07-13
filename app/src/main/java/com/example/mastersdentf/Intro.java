cd5c869257518e810e1209e2e1ae080d01333050
Steve <I1413735@continental.edu.pe>
2020-07-13 11:50:32 -0500

    Inteface Creada Login

---
+++ app/src/main/java/com/example/mastersdentf/Intro.java
@@ -0,0 +1,4 @@
+package com.example.mastersdentf;
+
+public class Intro {
+}
---
+++ app/src/main/java/com/example/mastersdentf/RecuperarContrasenna.java
@@ -0,0 +1,14 @@
+package com.example.mastersdentf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuperarContrasenna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_contrasenna);
+    }
+}
