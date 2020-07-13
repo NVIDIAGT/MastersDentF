04802d1c9d2009eb9f6efef1d1d63717a2a89e17
Steve <I1413735@continental.edu.pe>
2020-07-13 11:53:12 -0500

    splash Screem

--- app/src/main/java/com/example/mastersdentf/Intro.java
+++ app/src/main/java/com/example/mastersdentf/Intro.java
@@ -1,4 +1,25 @@
 package com.example.mastersdentf;
 
-public class Intro {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+public class Intro extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        Intent o = new Intent(this, IniciarSesion.class);
+        //timer procesos que toman 5 minutos
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(o);
+        finish();
+    }
 }
