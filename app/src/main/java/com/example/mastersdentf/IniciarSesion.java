043283465beda3dc63152d2669ec916e310d4603
Steve <I1413735@continental.edu.pe>
2020-07-13 20:23:09 -0500

    Inicio Y Registro campos validados

--- app/src/main/java/com/example/mastersdentf/IniciarSesion.java
+++ app/src/main/java/com/example/mastersdentf/IniciarSesion.java
@@ -1,19 +1,41 @@
 package com.example.mastersdentf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class IniciarSesion extends AppCompatActivity {
+    EditText C1,C2;
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        //Iniciar sesion
+        correo = findViewById(R.id.txtcorreo);
+        contraseña = findViewById(R.id.txtcontraseña);
+        C1=findViewById(R.id.txtCorreo);
+        C2=findViewById(R.id.txtContrasena);
+        mAuth = FirebaseAuth.getInstance();
+        //Iniciar sesion
 
         Button irARegistro = findViewById(R.id.btnRegistarse);
         irARegistro.setOnClickListener(new View.OnClickListener() {
@@ -23,24 +45,64 @@ public class IniciarSesion extends AppCompatActivity {
                 startActivity(o);
             }
         });
-        Button EstardentrodelAPP = findViewById(R.id.btnEntrar);
-        EstardentrodelAPP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent o = new Intent(IniciarSesion.this, MainActivity.class);
-                startActivity(o);
-            }
-        });
         //TXT Ir a recueprar contraseña
         TextView txtRecuperarContraseña = this.findViewById(R.id.txtRecuperarContraseña);
-        txtRecuperarContraseña.setOnClickListener(new View.OnClickListener()
-        {
+        txtRecuperarContraseña.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
                 Intent o = new Intent(IniciarSesion.this, RecuperarContrasenna.class);
                 startActivity(o);
             }
         });
-        //TXT Ir a recueprar contraseña
     }
-}
+    public boolean validar() {
+        boolean retorno = true;
+        String C1 = correo.getText().toString();
+        String C2 = correo.getText().toString();
+        if (C1.isEmpty()) {
+            correo.setError("El campo del correo electronico esta vacio");
+            retorno = false;
+        }
+        if (C2.isEmpty()) {
+            contraseña.setError("El campo de la contraseña esta vacio");
+            retorno = false;
+        }
+        return retorno;
+    }
+    //Iniciar sesion
+    public void iniciarSesion(View v){
+      if(validar()){
+          mAuth.signInWithEmailAndPassword(correo.getText().toString(), contraseña.getText().toString())
+                  .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                      @Override
+                      public void onComplete(@NonNull Task<AuthResult> task) {
+                          if (task.isSuccessful()) {
+                              // Sign in success, update UI with the signed-in user's information
+                              //Log.d(TAG, "signInWithEmail:success");
+                              FirebaseUser user = mAuth.getCurrentUser();
+                              // updateUI(user);
+
+                              Intent o = new Intent(IniciarSesion.this, MainActivity.class);
+                              startActivity(o);
+                          } else {
+                              // If sign in fails, display a message to the user.
+                              //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                              Toast.makeText(getApplicationContext(), "Usuario y contraseña incorrectos.",
+                                      Toast.LENGTH_SHORT).show();
+                              //updateUI(null);
+                          }
+                          // ...
+                      }
+                  });
+      }
+    }
+    //fin Iniciar sesion
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    }
+
--- app/src/main/java/com/example/mastersdentf/RegistroUsuario.java
+++ app/src/main/java/com/example/mastersdentf/RegistroUsuario.java
@@ -18,7 +18,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class RegistroUsuario extends AppCompatActivity {
     private FirebaseAuth mAuth;
-
+    EditText C1,C2,C3,C4,C5;
     //campos de registro
     private EditText Nombre;
     private EditText Apellidos;
@@ -36,7 +36,41 @@ public class RegistroUsuario extends AppCompatActivity {
         Correo = findViewById(R.id.txtCorreo);
         Contraseña = findViewById(R.id.txtContrasena);
         ConfirmarContraseña = findViewById(R.id.txtConfirmarContraseña);
+        C1=findViewById(R.id.txtNombre);
+        C2=findViewById(R.id.txtApellidos);
+        C3=findViewById(R.id.txtCorreo);
+        C4=findViewById(R.id.txtContrasena);
+        C5=findViewById(R.id.txtConfirmarContraseña);
 
+    }
+    public boolean validar() {
+        boolean retorno = true;
+        String C1 = Nombre.getText().toString();
+        String C2 = Apellidos.getText().toString();
+        String C3 = Correo.getText().toString();
+        String C4 = Contraseña.getText().toString();
+        String C5 = ConfirmarContraseña.getText().toString();
+        if (C1.isEmpty()) {
+            Nombre.setError("El campo del correo electronico esta vacio");
+            retorno = false;
+        }
+        if (C2.isEmpty()) {
+            Apellidos.setError("El campo del contraseña esta vacio");
+            retorno = false;
+        }
+        if (C3.isEmpty()) {
+            Correo.setError("El campo del correo electronico esta vacio");
+            retorno = false;
+        }
+        if (C4.isEmpty()) {
+            Contraseña.setError("El campo del correo electronico esta vacio");
+            retorno = false;
+        }
+        if (C5.isEmpty()) {
+            ConfirmarContraseña.setError("El campo del correo electronico esta vacio");
+            retorno = false;
+        }
+        return retorno;
     }
    //verificar si inicio sesion
    @Override
@@ -47,33 +81,33 @@ public class RegistroUsuario extends AppCompatActivity {
        //updateUI(currentUser);
    }
    //registrar Usuario Nuevo
-    public void registrarUsuario(View view){
-        if( Contraseña.getText().toString().equals(ConfirmarContraseña.getText().toString())){
-            mAuth.createUserWithEmailAndPassword( Correo.getText().toString(), Contraseña.getText().toString())
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                //Log.d(TAG, "createUserWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                //updateUI(user);
-                                Intent o = new Intent(RegistroUsuario.this, IniciarSesion.class);
-                                startActivity(o);
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                //Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                Toast.makeText(getApplicationContext(), "Authentication failed.",Toast.LENGTH_SHORT).show();
-                                //updateUI(null);
+    public void registrarUsuario(View view) {
+        if (validar()) {
+            if (Contraseña.getText().toString().trim().equals(ConfirmarContraseña.getText().toString().trim())) {
+                mAuth.createUserWithEmailAndPassword(Correo.getText().toString().trim(), Contraseña.getText().toString().trim())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    //Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    //updateUI(user);
+                                    Intent o = new Intent(RegistroUsuario.this, IniciarSesion.class);
+                                    startActivity(o);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+
+                                // ...
                             }
-
-                            // ...
-                        }
-                    });
-        }else{
-            Toast.makeText(this, "Las contraseñas no coinciden",Toast.LENGTH_SHORT).show();
+                        });
+            } else {
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            }
         }
-
-
     }
 }
